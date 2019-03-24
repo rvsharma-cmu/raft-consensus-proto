942dcbed9ea61555f9989d8511788a2b71d9702c
akankshaBubber <akanksha.bubber@gmail.com>
2019-03-24 03:17:52 -0400

    Verify once

--- runTests.cmd
+++ runTests.cmd
@@ -3,43 +3,43 @@ for i in 1 2 3 4 5
 do
 	if java RaftTest Initial-Election 8000 | grep -q 'Passed'; then
    		echo "matched Initial-Election"
-	fi
 	else
 		echo "No matched Initial-Election"
+	fi
 	if java RaftTest Re-Election 8000 | grep -q 'Passed'; then
    		echo "matched Re-Election"
-	fi
 	else
                 echo "No matched Re-Election"
+	fi
 	if java RaftTest Basic-Agree 8000 | grep -q 'Passed'; then
    		echo "matched Basic-Agree"
-	fi
 	else
 		echo "NO matched Basic-Agree"
+	fi
 	if java RaftTest Fail-Agree 8000 | grep -q 'Passed'; then
    		echo "matched Fail-Agree"
-	fi
 	else
 		echo "NO matched Fail-Agree"
+	fi
 	if java RaftTest Fail-NoAgree 8000 | grep -q 'Passed'; then
    		echo "matched Fail-NoAgree"
-	fi
 	else
 		echo "No matched Fail-NoAgree"
+	fi
 	if java RaftTest Rejoin 8000 | grep -q 'Passed'; then
    		echo "matched Rejoin"
-	fi
 	else
 		echo "No matched Rejoin"
+	fi
 	if java RaftTest Backup 8000 | grep -q 'Passed'; then
    		echo "matched Backup"
-	fi
 	else
 		echo "No matched Backup"
+	fi
 	if java RaftTest Count 8000 | grep -q 'Passed'; then
    		echo "matched Count"
-	fi
 	else
 		echo "No matched Count"
+	fi
 done
 
